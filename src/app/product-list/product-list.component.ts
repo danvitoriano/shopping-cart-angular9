ce47ef9f6c778e4106340d736d1f27702a6caaa2
Danilo Vitoriano <vitoriano08@gmail.com>
2020-09-24 17:19:19 -0300

    diretiva for, componentes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,27 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
-  }
-
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale');
+    window.alert("The product has been shared!");
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,24 +1,17 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: "Phone XL",
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: "A large phone with one of the best screens",
   },
   {
-    name: 'Phone Mini',
+    name: "Phone Mini",
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: "A great phone with one of the best cameras",
   },
   {
-    name: 'Phone Standard',
+    name: "Phone Standard",
     price: 299,
-    description: ''
-  }
+    description: "",
+  },
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
